2a933f31df35a256a8208fca54ce834006f0c96b
Malhar-Soomro <malharsoomro2@gmail.com>
2025-05-12 16:43:33 +0500

    updated error messages for todo requests

--- routes/todo.ts
+++ routes/todo.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from "express";
+import express, {Request, Response} from "express";
 import Todo from "../models/Todo";
 import verifyUser from "../middleware/verifyUser";
 
 const router = express.Router();
 
 router.post("/", verifyUser, async (req: Request, res: Response) => {
-
   const newTodo = new Todo({
     userId: req.user.id,
     title: req.body.title,
@@ -16,44 +15,39 @@ router.post("/", verifyUser, async (req: Request, res: Response) => {
     const savedTodo = await newTodo.save();
     res.status(201).json(savedTodo);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({error});
   }
 });
 
 router.get("/", verifyUser, async (req: Request, res: Response) => {
   try {
-
-    const todos = await Todo.find({ userId: req.user.id });
-
-    // if there is no any todo?
+    const todos = await Todo.find({userId: req.user.id});
 
     res.status(201).json(todos);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({error});
   }
 });
 
-
 router.delete("/:todoId", verifyUser, async (req: Request, res: Response) => {
   try {
-
     const todo = await Todo.findById(req.params.todoId);
 
     if (!todo) {
-      res.status(404).json({ message: "Todo not found" });
+      res.status(404).json({message: "Todo not found"});
       return;
     }
 
     // if userId is associated with that todo then delete it otherwise return
     if (todo.userId !== req.user.id) {
-      res.status(403).json({ message: "Not authorized to delete" });
+      res.status(403).json({message: "Not authorized to delete"});
       return;
     }
 
     await todo.deleteOne();
-    res.status(200).json({ message: "Todo deleted successfully" });
+    res.status(200).json({message: "Todo deleted successfully"});
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({error});
   }
 });
 
@@ -62,24 +56,24 @@ router.put("/:todoId", verifyUser, async (req: Request, res: Response) => {
     const todo = await Todo.findById(req.params.todoId);
 
     if (!todo) {
-      res.status(404).json({ message: "Todo not found" });
+      res.status(404).json({message: "Todo not found"});
       return;
     }
 
     if (todo.userId !== req.user.id) {
-      res.status(403).json({ message: "Unauthorized" });
+      res.status(403).json({message: "Unauthorized"});
       return;
     }
 
     const updatedTodo = await Todo.findByIdAndUpdate(
       req.params.todoId,
-      { $set: req.body },
-      { new: true }
+      {$set: {...req.body}},
+      {new: true}
     );
     res.status(200).json(updatedTodo);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({error});
   }
-})
+});
 
 module.exports = router;
