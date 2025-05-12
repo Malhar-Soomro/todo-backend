35ee7892378f39fc3e39090ae70ab9090732c0bd
Malhar-Soomro <malharsoomro2@gmail.com>
2025-05-12 16:41:54 +0500

    updated error messages for auth requests

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-import User from "../models/User";
+import express, {Request, Response} from "express";
+import User, {IUser} from "../models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Generate Token
 const generateToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SEC || "secret", { expiresIn: "1d" });
+  return jwt.sign({id}, process.env.JWT_SEC || "secret", {expiresIn: "1d"});
 };
 
 // register
@@ -15,53 +15,78 @@ router.post("/register", async (req: Request, res: Response) => {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
-  const newUser = new User({
+  try {
+
+    const user = await User.findOne({email: req.body.email});
+
+    if(user){
+      res.status(500).json({message:"Already registered"});
+      return;
+    }
+
+    const newUser = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     password: hashedPassword,
   });
 
-  try {
+
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
-    res.status(500).json(error);
+    console.log("error", error);
+    res.status(500).json({error});
   }
 });
 
 // login
 router.post("/login", async (req: Request, res: Response) => {
-
-  console.log(req.body);
-
   try {
     const user = await User.findOne({email: req.body.email});
 
     if (!user) {
-       res.status(401).json("user not found");
-       return;
+      res.status(401).json({message: "user not found"});
+      return;
     }
 
     // compare the passwords
     const isMatch = await bcrypt.compare(req.body.password, user.password);
 
     if (!isMatch) {
-        res.status(401).json("Wrong Credentials");
-        return;
+      res.status(401).json({message: "Wrong Credentials"});
+      return;
     }
 
     // generate token
     const token = generateToken(user._id.toString());
 
-    const { password, ...others } = user.toObject();
-    res.status(200).json({ ...others, token });
-    return; 
-    
-  } catch (error) {
-    console.log("error ->", error);
-    res.status(500).json(error);
+    const {password, ...others} = user.toObject();
+    res.status(200).json({...others, token});
     return;
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({error});
+    return;
+  }
+});
+
+router.get("/user", async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      const tokenData: any = jwt.verify(token, process.env.JWT_SEC as string);
+
+      const user = await User.findById(tokenData.id);
+
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({message: "You are not authenticated"});
+      return;
+    }
+  } catch (error) {
+    res.status(400).json({error});
   }
 });
 
